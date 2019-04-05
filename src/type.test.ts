70fa2117181051bf8d8af26037380a92ba90012f
tleef <the.tleef@gmail.com>
2019-04-05 18:58:41 -0300

    Add isBoolean and isInteger

--- src/type.test.ts
+++ src/type.test.ts
@@ -1,30 +1,81 @@
 import * as chai from "chai";
 
-import type, { isFunction, isNumber, isObject, isRegExp, isString } from "./type";
+import type, {
+  isBoolean,
+  isFunction,
+  isInteger,
+  isNumber,
+  isObject,
+  isRegExp,
+  isString
+} from "./type";
 
 const expect = chai.expect;
 
 describe("type", () => {
-  describe("isString()", () => {
-    it("should return true for string literal", () => {
-      expect(isString("test")).to.equal(true);
+  describe("isBoolean()", () => {
+    it("should return true for true", () => {
+      expect(isBoolean(true)).to.equal(true);
     });
 
-    it("should return true for template literal", () => {
-      expect(isString(`test ${1}`)).to.equal(true);
-      expect(isString(
-        `test ${1}
-        test ${2}`,
-      )).to.equal(true);
+    it("should return true for false", () => {
+      expect(isBoolean(false)).to.equal(true);
     });
 
-    it("should return true for String", () => {
-      expect(isString(String("test"))).to.equal(true);
-      expect(isString(String(123))).to.equal(true);
+    it("should return true for Boolean", () => {
+      expect(isBoolean(Boolean(true))).to.equal(true);
+      expect(isBoolean(Boolean(false))).to.equal(true);
     });
 
-    it("should return false for new String", () => {
-      expect(isString(new String("test"))).to.equal(false); // eslint-disable-line
+    it("should return false for new Boolean", () => {
+      expect(isBoolean(new Boolean(true))).to.equal(false);
+      expect(isBoolean(new Boolean(false))).to.equal(false);
+    });
+  });
+
+  describe("isFunction()", () => {
+    it("should return true for function literal", () => {
+      expect(isFunction(function() {})).to.equal(true);
+    });
+
+    it("should return true for lambda literal", () => {
+      expect(isFunction(() => {})).to.equal(true);
+    });
+
+    it("should return true for Function", () => {
+      expect(isFunction(Function())).to.equal(true); // eslint-disable-line
+    });
+
+    it("should return true for new Function", () => {
+      expect(isFunction(new Function())).to.equal(true); // eslint-disable-line
+    });
+  });
+
+  describe("isInteger()", () => {
+    it("should return true for int literal", () => {
+      expect(isInteger(123)).to.equal(true);
+    });
+
+    it ('should return false for float', () => {
+      expect(isInteger(123.000001)).to.equal(false);
+    });
+
+    it("should return true for Number", () => {
+      expect(isInteger(Number(123))).to.equal(true);
+      expect(isInteger(Number("123"))).to.equal(true);
+    });
+
+    it("should return false for new Number", () => {
+      expect(isInteger(new Number(123))).to.equal(false); // eslint-disable-line
+    });
+
+    it("should return false for NaN", () => {
+      expect(isInteger(NaN)).to.equal(false);
+    });
+
+    it("should return false for Infinity", () => {
+      expect(isInteger(Infinity)).to.equal(false);
+      expect(isInteger(-Infinity)).to.equal(false);
     });
   });
 
@@ -94,24 +145,6 @@ describe("type", () => {
     });
   });
 
-  describe("isFunction()", () => {
-    it("should return true for function literal", () => {
-      expect(isFunction(function() {})).to.equal(true);
-    });
-
-    it("should return true for lambda literal", () => {
-      expect(isFunction(() => {})).to.equal(true);
-    });
-
-    it("should return true for Function", () => {
-      expect(isFunction(Function())).to.equal(true); // eslint-disable-line
-    });
-
-    it("should return true for new Function", () => {
-      expect(isFunction(new Function())).to.equal(true); // eslint-disable-line
-    });
-  });
-
   describe("isRegExp()", () => {
     it("should return true for regexp literal", () => {
       expect(isRegExp(/\w+/)).to.equal(true);
@@ -122,9 +155,44 @@ describe("type", () => {
     });
   });
 
-  describe("#isString()", () => {
-    it("should be equal to isString", () => {
-      expect(type.isString).to.equal(isString);
+  describe("isString()", () => {
+    it("should return true for string literal", () => {
+      expect(isString("test")).to.equal(true);
+    });
+
+    it("should return true for template literal", () => {
+      expect(isString(`test ${1}`)).to.equal(true);
+      expect(isString(
+          `test ${1}
+        test ${2}`,
+      )).to.equal(true);
+    });
+
+    it("should return true for String", () => {
+      expect(isString(String("test"))).to.equal(true);
+      expect(isString(String(123))).to.equal(true);
+    });
+
+    it("should return false for new String", () => {
+      expect(isString(new String("test"))).to.equal(false); // eslint-disable-line
+    });
+  });
+
+  describe("#isBoolean()", () => {
+    it("should be equal to isBoolean", () => {
+      expect(type.isBoolean).to.equal(isBoolean);
+    });
+  });
+
+  describe("#isFunction()", () => {
+    it("should be equal to isFunction", () => {
+      expect(type.isFunction).to.equal(isFunction);
+    });
+  });
+
+  describe("#isInteger()", () => {
+    it("should be equal to isInteger", () => {
+      expect(type.isInteger).to.equal(isInteger);
     });
   });
 
@@ -140,15 +208,15 @@ describe("type", () => {
     });
   });
 
-  describe("#isFunction()", () => {
-    it("should be equal to isFunction", () => {
-      expect(type.isFunction).to.equal(isFunction);
-    });
-  });
-
   describe("#isRegExp()", () => {
     it("should be equal to isRegExp", () => {
       expect(type.isRegExp).to.equal(isRegExp);
+    });
+  });
+
+  describe("#isString()", () => {
+    it("should be equal to isString", () => {
+      expect(type.isString).to.equal(isString);
     });
   });
 });
--- src/type.ts
+++ src/type.ts
@@ -1,5 +1,13 @@
-const isString = (o: any): boolean => {
-  return typeof o === "string";
+const isBoolean = (o: any): boolean => {
+  return o === true || o === false;
+};
+
+const isFunction = (o: any): boolean => {
+  return typeof o === "function";
+};
+
+const isInteger = (o: any): boolean => {
+  return Number.isInteger(o);
 };
 
 const isNumber = (o: any): boolean => {
@@ -10,16 +18,18 @@ const isObject = (o: any): boolean => {
   return typeof o === "object" && !!o && !Array.isArray(o);
 };
 
-const isFunction = (o: any): boolean => {
-  return typeof o === "function";
-};
-
 const isRegExp = (o: any): boolean => {
   return o instanceof RegExp;
 };
 
+const isString = (o: any): boolean => {
+  return typeof o === "string";
+};
+
 export default {
+  isBoolean,
   isFunction,
+  isInteger,
   isNumber,
   isObject,
   isRegExp,
@@ -27,7 +37,9 @@ export default {
 };
 
 export {
+  isBoolean,
   isFunction,
+  isInteger,
   isNumber,
   isObject,
   isRegExp,
